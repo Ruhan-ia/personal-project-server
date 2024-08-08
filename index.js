fef270d8d74fb418604dc7e5f72c80099ae1d3e0
Istaik Ahamad <ahmedruhanbmarpc@gmail.com>
2024-08-09 00:02:24 +0600

    payment methode intregate

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app =express();
 const cors = require("cors");
 const jwt = require('jsonwebtoken');
+const stripe = require('stripe')(process.env.STRIPE_SECRET_TOKEN)
 require('dotenv').config()
 const port = process.env.PORT || 5000;
 
@@ -157,6 +158,20 @@ const verifyAdmin = async(req, res, next) =>{
     console.log(id)
     const result = await cartsDetails.deleteOne(query);
     res.send(result);
+  })
+  app.post('/create-payment-intent', async (req, res) =>{
+    const {price} = req.body;
+    const amount = parseInt(price * 100);
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount : amount,
+      currency: "usd",
+      
+      payment_method_types:['card']
+    })
+
+    res.send({
+      clientSecret: paymentIntent.client_secret,
+    })
   })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
